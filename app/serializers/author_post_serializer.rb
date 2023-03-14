63f7fc4fa78555e4f73473872c0ba99b2356837c
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-13 21:01:23 -0400

    lab complete

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,17 +1,10 @@
 class AuthorPostSerializer < ActiveModel::Serializer
   attributes :title, :short_content
-<<<<<<< HEAD
   belongs_to :author
   has_many :tags
 
   def short_content
-    "#{object.content[0..39]}..."
-=======
-  has_many :tags
-
-  def short_content
     "#{object.content[0..39]...}"
->>>>>>> bddc5d6 (lab complete)
   end
 
 end
--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,8 +1,4 @@
 class TagSerializer < ActiveModel::Serializer
   attributes :name
-<<<<<<< HEAD
-=======
-
->>>>>>> bddc5d6 (lab complete)
   has_many :posts
 end
