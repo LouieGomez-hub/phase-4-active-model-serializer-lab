434af67cbba939f06e24fc75bfea1aebfe4dff51
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-13 20:57:49 -0400

    lab complete

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -1,5 +1,10 @@
 class AuthorSerializer < ActiveModel::Serializer
   attributes :name
+<<<<<<< HEAD
   has_one :profile
   has_many :posts, serializer AuthorPostSerializer
+=======
+  has_one :profile, serializer :AuthorProfileSerializer
+  has_many :posts, serializer :AuthorPostSerializer
+>>>>>>> bddc5d6 (lab complete)
 end
--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,4 +1,8 @@
 class TagSerializer < ActiveModel::Serializer
   attributes :name
+<<<<<<< HEAD
+=======
+
+>>>>>>> bddc5d6 (lab complete)
   has_many :posts
 end
